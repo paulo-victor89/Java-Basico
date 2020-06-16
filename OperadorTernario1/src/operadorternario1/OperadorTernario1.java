88c8ca6603769f582c9f373973fc891887cdbada
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-06-16 16:53:03 -0300

    Operador ternario

---
+++ OperadorTernario1/src/operadorternario1/OperadorTernario1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorternario1;
+
+/**
+ *
+ * @author victor
+ */
+public class OperadorTernario1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       	int n1, n2, resultado;
+        n1 = 14;
+        n2 = 18;
+        resultado = (n1 > n2) ? n1 : n2;
+        System.out.println(resultado);
+    }
+    
+}

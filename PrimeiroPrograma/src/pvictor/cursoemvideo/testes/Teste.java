a5377a0cd2c4cd3aa3de205a7d6783c04ab06f07
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-06-18 14:35:57 -0300

    Adicionando uma classe de testes

---
+++ PrimeiroPrograma/src/pvictor/cursoemvideo/testes/Teste.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pvictor.cursoemvideo.testes;
+
+/**
+ *
+ * @author victor
+ */
+public class Teste {
+
+    char c = 'c';
+    int i = 10;
+    double d = 10;
+    long l = 1;
+    String s = "Hello";
+
+//    c = c +1;
+//    s += 1;
+//    i += s ;
+//    c +=s;
+//    i+=l;
+
+}

8ac7f1b7d529c7cb9263f1f0e8f223ac0a4a9247
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-06-18 17:31:13 -0300

    Repeticao For, contagem de numero com intervalo

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author victor
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for(int cc=1; cc<=100; cc+=10){// contando de 1 ate 100 pulando de 10 em 10 
+           System.out.println(cc);
+       }
+    }
+    
+}
